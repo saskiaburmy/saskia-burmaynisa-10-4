f29b56a28ee11e3f77e3b373a8ff68dc98e9375b
saskiaburmy <saskiaburrmeyy@gmail.com>
2026-03-14 19:49:25 +0700

    mengisi biodata portofolio

---
+++ tugas portofolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Portofolio() {
+  return (
+    <div>
+      <h1>Portofolio Saskia Burmaynisa</h1>
+      <p>Halo, saya siswi kelas X4. Ini adalah tugas koding pertama saya.</p>
+      <p>Saya sedang belajar membuat website menggunakan React.js.</p>
+    </div>
+  )
+}
+
+export default Portofolio
